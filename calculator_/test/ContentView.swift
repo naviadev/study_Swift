94f59be10f0130569a6f30ba505f3b86578e9c54
naviadev <166151262+naviadev@users.noreply.github.com>
2024-04-30 10:37:01 +0900

    Update ContentView.swift

--- calculator_/test/ContentView.swift
+++ calculator_/test/ContentView.swift
@@ -7,14 +7,22 @@
 
 import SwiftUI
 
+// 메인뷰
 struct ContentView : View{
+    // * result : 결과 값을 가지고 있다. equal 입력 시 해당 변수에 값을 넣는 형태. 마지막에 해당값을 통해 recent :String으로 변환 후 출력 
     @State var result : Int = 0
+    // * 현재 입력값 위에 보이는 작은 텍스트 -> 지금까지 입력한 값과 기호를 출력해준다.
     @State var subText : String = ""
+    // * 현재 입력중인 값
     @State var recent : String = ""
+    // * 부호
     @State var sign : String = ""
+    // * 튜플로 변경해야할까? 
     @State var resultArr : Array = []
     var exeCalculator = false;
     @State var firstInput = true;
+    // 동적으로 버튼을 생성하기 위해 배열을 Vstack을 내부에서 ForEach 사용
+    // ForEach -> 뷰를 대상으로 작동하는 구조체
     var buttons = [
         ["AC", "+/-", "%", "/"],
         ["7","8","9","X"],
